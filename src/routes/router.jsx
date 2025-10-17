4b39bd7c0d87a4d2ea16a316f2c6fc04a0ce1731
md mehedi hassan <mdmehedi01326018868@gmail.com>
2025-10-17 23:58:09 +0600

    added header nav and main

---
+++ src/routes/router.jsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layouts/HomeLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout></HomeLayout>
+  },
+  {
+    path: "/auth",
+    element: <h2>authentication layout</h2>,
+  },
+  {
+    path: "/news",
+    element: <h2>news layout</h2>,
+  },
+  {
+    path: "/*",
+    element: <h2>error layout</h2>,
+  },
+]);
+export default router;
